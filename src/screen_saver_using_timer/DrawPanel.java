6f7358ba11ebe49b578260de42e11f4e945e4284
Arif Gencosmanoglu <a.gencosmanoglu@gmail.com>
2016-10-26 12:45:16 -0400

    Added Team Project #3 (Screen Saver)

--- src/screen_saver_using_timer/DrawPanel.java
+++ src/screen_saver_using_timer/DrawPanel.java
@@ -1,18 +1,10 @@
 package screen_saver_using_timer;
 
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-//Fig. 3.16: DrawPanel.java
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.Shape;
-import java.awt.image.ImageObserver;
 import java.security.SecureRandom;
-import java.text.AttributedCharacterIterator;
+import java.util.ArrayList;
 
-import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 // Using drawLine to connect the corners of a panel.
@@ -20,39 +12,63 @@ public class DrawPanel extends JPanel
 {
 	// create secure random number generator for use in method rollDice
 	private static final SecureRandom randomNumbers = new SecureRandom();
-		
+	
+	// the maximum amount of lines that the draw-panel can contain
+	public int MAX_LINES = 100;
+	
+	// the length and width of the draw-panel
 	private int width, height;
-	private Graphics graph;
-		
+	// the array which holds all the lines
+	private ArrayList<Line> lines;
 	
 	public DrawPanel()
 	{
-		
+		// initialize the lines array
+		lines = new ArrayList<Line>();
 	}
+	
+	// set the maximum lines the the draw-panel can contain
+	public void setMaximumLines(int maxLines)
+	{
+		MAX_LINES = maxLines;
+	}
+	
 	// draws an X from the corners of the panel
 	public void paintComponent(Graphics g)
 	{
 		// call paintComponent to ensure the panel displays correctly
-		graph = g;
-				
-		super.paintComponent(graph);
+		super.paintComponent(g);
 		
 		width = getWidth(); // total width
 		height = getHeight(); // total height
 		
-		DrawRandomLine();
+		// every time this method is invoked, add a random line
+		addRandomLine();
+		
+		// draw all the lines
+		if(lines.size() == MAX_LINES)
+		{
+			lines.clear(); // clears all the lines inside the array
+		}
+		
+		for(Line l : lines)
+		{
+			g.setColor(l.getColor()); // set the color of the line
+			g.drawLine(l.getX1(), l.getY1(), l.getX2(), l.getY2()); // set the coordinates of the line 
+		}
 	 }
 	
-	public void DrawRandomLine()
+	// add line to the array that is randomly initialized
+	private void addRandomLine()
 	{
-		int randomX1= 1 + randomNumbers.nextInt(width); // first die roll
-		int randomX2= 1 + randomNumbers.nextInt(width); // first die roll
-		int randomY1= 1 + randomNumbers.nextInt(height); // first die roll
-		int randomY2= 1 + randomNumbers.nextInt(height); // first die roll
+		// create random coordinates
+		int randomX1= 1 + randomNumbers.nextInt(width); 
+		int randomX2= 1 + randomNumbers.nextInt(width);
+		int randomY1= 1 + randomNumbers.nextInt(height);
+		int randomY2= 1 + randomNumbers.nextInt(height);
 		
-		// draw a line from the upper-left to the lower-right
-		graph.drawLine(randomX1, randomY1, randomX2, randomY2);
-		//graph.drawLine(0, 0, width, height);
+		// add the randomly initialized line into the array
+		lines.add(new Line(randomX1, randomY1, randomX2, randomY2, Color.BLACK));
 	}
 } // end class DrawPanel
 
---
+++ src/screen_saver_using_timer/Test_ScreenSaver.java
@@ -0,0 +1,14 @@
+package screen_saver_using_timer;
+
+public class Test_ScreenSaver {
+	public static void main(String[] args)
+	{
+		// initialize the screen saver
+		ScreenSaver ss = new ScreenSaver();
+		// define the settings for the screen saver
+		ss.setMaximumLines(100);
+		ss.setTimerInterval(100);
+		// start the screen saver
+		ss.startScreenSaver();
+	} 
+}
